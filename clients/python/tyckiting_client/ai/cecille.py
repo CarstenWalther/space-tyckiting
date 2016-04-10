7d2de718df5f75e78353bbba76e23660e32b7c97
Carsten Walther <carsten.walther@student.hpi.de>
2016-04-10 12:17:57 +0200

    finished triangle shot

--- clients/python/tyckiting_client/ai/cecille.py
+++ clients/python/tyckiting_client/ai/cecille.py
@@ -10,17 +10,23 @@ Else Move if in danger
 Else shoot at first target if available
 Else scan random field
 '''
-
+from tyckiting_client.ai.strategies.triangleShot import TriangleShot
 
 class Ai(base.BaseAi):
+
+    def __init__(self, team_id, config=None):
+        super(Ai, self).__init__(team_id, config)
+        self.triShot = TriangleShot()
 
     def move(self, bots, events):
         response = []
         bots = list(bots)
-
-        targetsPos, endangered = self.analyzeEvents(events)
+        events = list(events)
+        targetsPos = self.triShot.findTargets(events)
+        additionalTargets, endangered = self.analyzeEvents(events)
+        targetsPos += additionalTargets
         if self.livingBotCount(bots) == 3 and len(targetsPos) > 0:
-            return self.triangleShot(bots, targetsPos[0])
+            return self.triShot.triangleShot(bots, targetsPos[0])
 
         for bot in bots:
             if not bot.alive:
@@ -37,16 +43,4 @@ class Ai(base.BaseAi):
             response.append(action)
         return response
 
-    def triangleShot(self, bots, targetPos):
-        logging.info('attempt triangle shot')
-        shotOffsets = [(-1,0), (1,-1), (0,1)]
-        shotCount = 0
-        response = []
-        for bot in bots:
-            if not bot.alive:
-                continue
-            response.append(actions.Cannon(bot_id=bot.bot_id,
-                x = targetPos.x + shotOffsets[shotCount][0],
-                y = targetPos.y + shotOffsets[shotCount][1]))
-            shotCount += 1
-        return response
+
---
+++ clients/python/tyckiting_client/ai/strategies/triangleShot.py
@@ -0,0 +1,70 @@
+
+import logging
+
+from tyckiting_client.hexagon import cube_add
+from tyckiting_client import actions
+from tyckiting_client import messages
+
+class TriangleShot(object):
+
+	def __init__(self):
+		self.shotOffsets = [(-1,0), (1,-1), (0,1)]
+		self.offsetsBySource = {}
+		self.center = None
+
+	def triangleShot(self, bots, targetPos):
+		logging.info('Attempt triangle shot at %s', targetPos)
+		self.offsetsBySource.clear()
+		shotCount = 0
+		response = []
+		self.center = (targetPos.x, targetPos.y)
+		for bot in bots:
+			if not bot.alive:
+				continue
+			x = targetPos.x + self.shotOffsets[shotCount][0]
+			y = targetPos.y + self.shotOffsets[shotCount][1]
+			response.append(actions.Cannon(bot_id=bot.bot_id, x=x, y=y))
+			self.offsetsBySource[bot.bot_id] = self.shotOffsets[shotCount]
+			shotCount += 1
+		return response
+
+	def findTargets(self, events):
+		targetsPos = []
+		if not self.center:
+			return targetsPos
+		shipsHit = self._getShipsHit(events)
+		for bot_id in shipsHit:
+			scoringSources = self._getSourcesHittingTarget(bot_id, events)
+			targetsPos += self._triangulate(scoringSources)
+		self.clear()
+		logging.info('Triangle shot found targets: %s', targetsPos)
+		return targetsPos
+
+	def _getShipsHit(self, events):
+		shipsHit = set()
+		for event in events:
+			if event.event == 'hit':
+				shipsHit.add(event.bot_id)
+		return shipsHit
+
+	def _getSourcesHittingTarget(self, target, events):
+		scoringSources = []
+		for event in events:
+			if event.event == 'hit' and event.bot_id == target:
+				scoringSources.append(event.source)
+		return scoringSources
+
+	def _triangulate(self, sources):
+		hits = len(sources)
+		if hits < 2:
+			return []
+		elif hits == 2:
+			pos = cube_add(self.center, self.offsetsBySource[sources[0]])
+			pos = cube_add(pos, self.offsetsBySource[sources[1]])
+			return [messages.Pos(pos[0], pos[1])]
+		elif hits == 3:
+			return [messages.Pos(self.center[0], self.center[1])]
+
+	def clear(self):
+		self.offsetsBySource.clear()
+		self.center = None
