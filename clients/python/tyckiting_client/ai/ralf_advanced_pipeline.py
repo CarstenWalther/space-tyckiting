1f473b42e5defc14481711fbb20c242003e25947
Balthasar M <balthasarmartin@web.de>
2016-04-22 19:29:16 +0200

    created advances pipeline escaping and some test bots

---
+++ clients/python/tyckiting_client/ai/ralf_advanced_pipeline.py
@@ -0,0 +1,74 @@
+import logging
+
+from tyckiting_client.ai import base
+from tyckiting_client import actions
+from tyckiting_client import hexagon
+from tyckiting_client.ai.strategies import escaping
+from tyckiting_client.ai.strategies import scanning
+from tyckiting_client.ai.strategies import shooting
+from tyckiting_client.ai.strategies import tracking
+
+'''
+Rules:
+like orania but escapes the right way
+'''
+
+class Ai(base.BaseAi):
+
+    def __init__(self, team_id, config=None):
+        super(Ai, self).__init__(team_id, config)
+        self.escaping = escaping.PipelineEscapingAdvanced(self.config)
+        self.scanning = scanning.StatisticalScanning(self.config)
+        self.tracking = tracking.Tracker(tracking.STRAIGHT_DISTANCE2_PATTERN)
+
+    def move(self, bots, events):
+        response = []
+        endangered = self.getEndangeredBots(events)
+        livingCount = self.livingBotCount(bots)
+        available = livingCount - len(endangered)
+        target = self.tracking.getTarget()
+
+        positionsNextRound = self.doPositioning(endangered, response, bots, target)
+
+        pendingTrackScan = False
+        if target and available >= 2:
+            pendingTrackScan = True
+            available -= 1
+
+        shooting = True
+        shootCoords = self.tracking.getShootCoordinates(available, positionsNextRound)
+        if not shootCoords:
+            shooting = False
+            scanCoords = self.scanning.getPossibleScanPositions(available)
+
+        for bot in bots:
+            if not bot.alive:
+                continue
+            if bot.bot_id in endangered:
+                continue
+            elif pendingTrackScan:
+                action = actions.Radar(bot_id=bot.bot_id, x=target[0], y=target[1])
+                pendingTrackScan = False
+            elif shooting:
+                shootCoord = shootCoords.pop()
+                action = actions.Cannon(bot_id=bot.bot_id, x=shootCoord[0], y=shootCoord[1])
+            else:
+                scanCoord = scanCoords.pop()
+                action = actions.Radar(bot_id=bot.bot_id, x=scanCoord[0], y=scanCoord[1])
+            response.append(action)
+        
+        return response
+
+    def doPositioning(self, botsToMove, response, bots, target):
+        positions = []
+        for bot in bots:
+            if not bot.alive:
+                continue
+            if bot.bot_id in botsToMove:
+                newPos = self.escaping.getMove(bot, bots, target)
+                action = actions.Move(bot_id=bot.bot_id, x=newPos.x, y=newPos.y)
+                response.append(action)
+                positions.append(newPos)
+            else:
+                positions.append(bot.pos)
+        return positions
--- clients/python/tyckiting_client/ai/strategies/escaping.py
+++ clients/python/tyckiting_client/ai/strategies/escaping.py
@@ -245,6 +245,71 @@ class PipelineEscaping(Escaping):
 		logging.info('Move %d from %s to %s', bot.bot_id, bot.pos, pos)
 		return pos
 
+class PipelineEscapingAdvanced(Escaping):
+
+	def __init__(self, config):
+		self.config = config
+		self.avoid_selfhit = AvoidSelfhit(self.config)
+		self.avoid_walls = AvoidWallsAdvanced(self.config)
+		self.straight = StraightDistance2Escaping(self.config)
+		self.curved = CurvedDistance2Escaping(self.config)
+		self.avoid_grouping = AvoidGrouping(self.config)
+
+	def getMove(self, bot, own_bots, target):
+		source = ""
+		final_coords = set()
+		found = False
+
+		if target and hexagon.distance(bot.pos, target) < 5:
+			self.avoid_selfhit.setEnemy(target)
+			coords = self.avoid_selfhit.getPossibleMoves(bot)
+			final_coords = coords
+			source += "avoid_selfhit; "
+
+		if hexagon.distance(bot.pos, (0,0)) > 10:
+			coords = self.avoid_walls.getPossibleMoves(bot)
+			if len(final_coords) == 0:
+				final_coords = coords
+				source += "avoid_walls; "
+			else:
+				new_coords = final_coords & coords
+				if len(new_coords) > 0:
+					final_coords = new_coords
+					source += "avoid_walls; "
+
+		distance_to_team = 32
+		bots = own_bots.copy()
+		bots.remove(bot)
+		for own_bot in bots:
+			if hexagon.distance(own_bot.pos, bot.pos) < distance_to_team:
+				distance_to_team = hexagon.distance(own_bot.pos, bot.pos)
+			if distance_to_team < 5:
+				self.avoid_grouping.setTeammates(bots)
+				coords = self.avoid_grouping.getPossibleMoves(bot)
+				if len(final_coords) == 0:
+					final_coords = coords
+					source += "avoid_grouping; "
+				else:
+					new_coords = final_coords & coords
+					if len(new_coords) > 0:
+						final_coords = new_coords
+						source += "avoid_grouping; "
+
+		if len(final_coords) == 0:
+				c1 = self.straight.getPossibleMoves(bot)
+				c2 = self.curved.getPossibleMoves(bot)
+				final_coords = c1 | c2
+				source += "two random; "
+		logging.info('Source is %s', source)
+
+
+		coord = random.choice(list(final_coords))
+		pos = Pos(coord[0], coord[1])
+		logging.info('Move %d from %s to %s', bot.bot_id, bot.pos, pos)
+		return pos
+
+
+
 
 
 class SpreadOwnBots(Escaping):
